8d4bb0bae42865c9ce3f5ce2bb0b4206db9634e6
HB <b5t511@163.com>
2023-07-05 20:02:06 +0800

    Fix player's rotation
    Change the the code that is relative to move forward and move right

--- Source/ActionRoguelike/Private/SCharacter.cpp
+++ Source/ActionRoguelike/Private/SCharacter.cpp
@@ -5,26 +5,42 @@
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
 #include "Components/InputComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 ASCharacter::ASCharacter()
 {
 	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>("SpringArmComp");
 	SpringArmComp->SetupAttachment(RootComponent);
+	SpringArmComp->bUsePawnControlRotation = true;
 
 	CameraComp = CreateDefaultSubobject<UCameraComponent>("CameraComp");
 	CameraComp->SetupAttachment(SpringArmComp);
+
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+
+	bUseControllerRotationYaw = false;
 }
 
 
 
 void ASCharacter::MoveForward(float Value)
 {
-	AddMovementInput(GetActorForwardVector(), Value);
+	FRotator ControlRotation = GetControlRotation();
+	ControlRotation.Roll = 0.0f;
+	ControlRotation.Pitch = 0.0f;
+
+	AddMovementInput(ControlRotation.Vector(), Value);
 }
 
 void ASCharacter::MoveRight(float Value)
 {
+	FRotator ControlRotation = GetControlRotation();
+	ControlRotation.Roll = 0.0f;
+	ControlRotation.Pitch = 0.0f;
+
+	FVector RightVector = FRotationMatrix(ControlRotation).GetScaledAxis(EAxis::Y);
+
 	AddMovementInput(GetActorRightVector(), Value);
 }
 
