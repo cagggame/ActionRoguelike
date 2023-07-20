6d8284ee9b623f0b0eae37216eff8a10d562db43
HB <b5t511@163.com>
2023-07-20 11:02:20 +0800

    Create projectile base class

--- Source/ActionRoguelike/Private/SProjectileBase.cpp
+++ Source/ActionRoguelike/Private/SProjectileBase.cpp
@@ -1,15 +1,15 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
 
-#include "SMagicProjectile.h"
+#include "SProjectileBase.h"
 #include "Components/SphereComponent.h"
 #include "Particles/ParticleSystemComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
 
 // Sets default values
-ASMagicProjectile::ASMagicProjectile()
+ASProjectileBase::ASProjectileBase()
 {
-	/*SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
+	SphereComp = CreateDefaultSubobject<USphereComponent>("SphereComp");
 	RootComponent = SphereComp;
 	SphereComp->SetCollisionProfileName("Projectile");
 
@@ -17,10 +17,7 @@ ASMagicProjectile::ASMagicProjectile()
 	ParticleComp->SetupAttachment(SphereComp);
 
 	MovementComp = CreateDefaultSubobject<UProjectileMovementComponent>("MovementComp");
-	MovementComp->InitialSpeed = 2000.0f;
 	MovementComp->bRotationFollowsVelocity = true;
-	MovementComp->bInitialVelocityInLocalSpace = true;*/
-
-	MovementComp->InitialSpeed = 2000.0f;
+	MovementComp->bInitialVelocityInLocalSpace = true;
 }
 
--- Source/ActionRoguelike/Public/SProjectileBase.h
+++ Source/ActionRoguelike/Public/SProjectileBase.h
@@ -4,29 +4,29 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Actor.h"
-#include "SProjectileBase.h"
-#include "SMagicProjectile.generated.h"
+#include "SProjectileBase.generated.h"
 
 class USphereComponent;
 class UParticleSystemComponent;
 class UProjectileMovementComponent;
 
 UCLASS()
-class ACTIONROGUELIKE_API ASMagicProjectile : public ASProjectileBase
+class ACTIONROGUELIKE_API ASProjectileBase : public AActor
 {
 	GENERATED_BODY()
 	
 public:	
 	// Sets default values for this actor's properties
-	ASMagicProjectile();
+	ASProjectileBase();
 
 protected:
-	/*UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly)
 	USphereComponent* SphereComp;
 
 	UPROPERTY(VisibleAnywhere)
 	UParticleSystemComponent* ParticleComp;
 
 	UPROPERTY(VisibleAnywhere)
-	UProjectileMovementComponent* MovementComp;*/
+	UProjectileMovementComponent* MovementComp;
+
 };
